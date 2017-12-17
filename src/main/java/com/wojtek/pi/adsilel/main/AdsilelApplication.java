364fa1c370ca00be2b4c92387265843419b766a8
Wojtek Pietrucha <wojtekpietrucha@gmail.com>
2017-12-17 17:12:11 +0100

    Initial commit

---
+++ src/main/java/com/wojtek/pi/adsilel/main/AdsilelApplication.java
@@ -0,0 +1,12 @@
+package com.wojtek.pi.adsilel.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsilelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdsilelApplication.class, args);
+	}
+}
---
+++ src/test/java/com/wojtek/pi/adsilel/main/AdsilelApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wojtek.pi.adsilel.main;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdsilelApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
