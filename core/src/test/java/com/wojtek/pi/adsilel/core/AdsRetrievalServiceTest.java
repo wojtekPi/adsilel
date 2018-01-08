c590a8cbb0ee9cb033d7fd3002d9ad569aafb0f9
Wojtek Pietrucha <wojtekpietrucha@gmail.com>
2018-01-08 01:02:11 +0100

    Fix tests to use them in JacoCo reporting

--- core/src/test/java/com/wojtek/pi/adsilel/core/AdsRetrievalServiceTest.java
+++ core/src/test/java/com/wojtek/pi/adsilel/core/AdsRetrievalServiceTest.java
@@ -1,18 +1,17 @@
 package com.wojtek.pi.adsilel.core;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 import org.mockito.Mock;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 import static java.time.LocalDateTime.parse;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 
-class AdsRetrievalServiceTest {
+public class AdsRetrievalServiceTest {
 
     private static final LocalDateTime FROM = parse("2007-12-03T10:15:30");
     private static final LocalDateTime TO = parse("2007-12-03T10:16:30");
@@ -26,25 +25,25 @@ class AdsRetrievalServiceTest {
 
     private AdsRetrievalService testedObject;
 
-    @BeforeEach
-    void setUp() {
+    @Before
+    public void setUp() {
         initMocks(this);
         testedObject = new AdsRetrievalService(rawAdsRetriever, rawAdsDao);
     }
 
     @Test
-    void shouldRetrieveAdsWhenRefreshCalled() {
+    public void shouldRetrieveAdsWhenRefreshCalled() {
         testedObject.refreshAds(FROM, TO);
 
         verify(rawAdsRetriever).fetchAds(FROM, TO);
     }
 
     @Test
-    void shouldSaveRawAdsToDao() {
+    public void shouldSaveRawAdsToDao() {
         when(rawAdsRetriever.fetchAds(FROM, TO)).thenReturn(rawAds);
 
         testedObject.refreshAds(FROM, TO);
 
-        verify(rawAdsDao).save(rawAds);
+        verify(rawAdsDao, times(1)).save(rawAds);
     }
 }
